3df1adce84702202c85fb7cc4cdc6597b46f3d9e
Raihan Rasheed Apurbo <apurbo97@gmail.com>
2021-04-18 14:19:48 +0600

    handling input commands

--- pipeline.cpp
+++ pipeline.cpp
@@ -34,30 +34,85 @@ istream & operator >> (istream &in,  Vector &c)
 
 }
 
+void printStringAscii(const string &s)
+{
+    for(int i=0;i<s.size();i++)
+    {
+        cout<<int(s[i])<<" ";
+    }
+    cout<<endl;
+}
+
 int main()
 {
     
-
+    // stage 1: Modeling Transformation
+    
     ifstream inputFile("scene.txt");
 
     Vector eye;
     inputFile>>eye;
-    cout<<eye;
+    // cout<<eye;
 
     Vector look;
     inputFile>>look;
-    cout<<look;
+    // cout<<look;
 
     Vector up;
     inputFile>>up;
-    cout<<up;
-
+    // cout<<up;
     double fovy,aspectRatio,near,far;
     inputFile>>fovy>>aspectRatio>>near>>far;
-    cout<<fovy<<endl;
-    cout<<aspectRatio<<endl;
-    cout<<near<<endl;
-    cout<<far<<endl;
+    // cout<<fovy<<endl;
+    // cout<<aspectRatio<<endl;
+    // cout<<near<<endl;
+    // cout<<far<<endl;
+    string s;
+    getline(inputFile,s);// after getting all input before first command
+
+    int counter = 0;
+    while(true)
+    {
+        getline(inputFile,s);
+        cout<<s<<endl;
+        // printStringAscii(s);
+        if(s.find("end") != std::string::npos)
+        {
+
+            cout<<"exiting from loop"<<endl;
+            break;
+        }
+        else if(s.find("triangle") != std::string::npos)
+        {
+            counter++;
+        }
+        else if(s.find("translate") != std::string::npos)
+        {
+            counter++;
+        }
+        else if(s.find("scale") != std::string::npos)
+        {
+            counter++;
+        }
+        else if(s.find("rotate") != std::string::npos)
+        {
+            counter++;
+        }
+        else if(s.find("push") != std::string::npos)
+        {
+            counter++;
+        }
+        else if(s.find("pop") != std::string::npos)
+        {
+            counter++;
+        }
+        // else
+        // {
+        //     cout<<"Something is wrong in input!"<<endl;
+        //     break;
+        // }   
+    }
+    cout<<counter<<" hey"<<endl;
 
 
     
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 g++ -o pipeline pipeline.cpp
 ./pipeline
+rm pipeline
 # ./a.out
